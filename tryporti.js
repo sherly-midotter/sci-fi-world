5912502a141531e0c39f9d9318bfb3a3d077e9f6
sherly <ianos.annemarie@gmail.com>
2023-03-09 22:01:50 +0100

    updated files

--- tryporti.js
+++ tryporti.js
@@ -2,10 +2,13 @@ window.addEventListener('load', function() {
     var loadingScreen = document.getElementById('loading-screen');
     var website = document.getElementById('website');
   
+    console.log('Loading screen shown');
+  
     // Remove the loading screen after the GIF has finished playing
     setTimeout(function() {
       loadingScreen.classList.add('loading-done');
       website.style.display = 'block';
-    }, 3000); // Change this value to match the duration of your GIF
+      console.log('Loading screen hidden');
+    }, 2000); // Change this value to match the duration of your GIF
   });
   
